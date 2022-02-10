c86f3979155e54f3706213e669c5069003ccb73c
sangheum <68062948+Kagi123@users.noreply.github.com>
2022-02-10 10:16:40 -0500

    Add files via upload

---
+++ edit-array.cpp
@@ -0,0 +1,41 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: edit-array.cpp
+
+creates an array of 10 integers, 
+and provides the user with an interface 
+to edit any of its elements. 
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+     int myData[10];
+    for (int i = 0; i < 10; i++) {
+        myData[i] = 1;
+     }
+     int ind;
+     int val;
+     do{
+       for (int i = 0; i < 10; i++) {
+        myData[i];
+        cout<< myData[i] << '\t';
+        } cout<< endl;
+
+       cout<< "Enter index: ";
+       cin >> ind;
+       cout<<"Enter value";
+       cin >> val;
+
+       if(ind>=0 && ind<10){
+         myData[ind] = val;
+       } else cout<< "index out of range";
+
+     }while(ind>=0 && ind<10);
+    
+}
+
+
