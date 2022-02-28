5c7cbd2476e1954503050c74a730550e5bcfa160
unknown <spark26@angelo.edu>
2022-02-28 10:24:28 -0500

    update

---
+++ inc_dec_neither.cpp
@@ -0,0 +1,34 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: inc_dec_neither.cpp
+
+Increasing, Decreasing or Neither
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  int num1,num2,num3;
+  cout<<"Enter a number1: "<<endl;
+  cin>> num1;
+   cout<<"Enter a number2: "<<endl;
+  cin>> num2;
+   cout<<"Enter a number3: "<<endl;
+  cin>> num3;
+  if(num1<num2 && num2< num3){
+    cout<<"increasing";
+  } else if(num1>num2 && num2 > num3){
+    cout<<"decreasing";
+  } else cout<<"neither";
+  
+}
+   
+
+
+
---
+++ positive-zero-negative.cpp
@@ -0,0 +1,34 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: Positive or Zero or Negative.cpp
+
+asks the user to input 
+find the number is
+Positive or Zero or Negative
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+  int num;
+  cout<<"Enter a number: "<<endl;
+  cin>> num;
+  if(num>0){
+    cout<<"positive";
+  } else if(num == 0){
+    cout<<"zero";
+  } else if (num <0){
+    cout<<"negative";
+  }
+  
+}
+   
+
+
+
