0db8c0ca51a59ea8a25a9bf8e15ba18cdbde13b8
unknown <spark26@angelo.edu>
2022-03-04 15:02:30 -0500

    update

---
+++ checkerboard3x3.cpp
@@ -0,0 +1,37 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: checkerboard3x3.cpp
+asks the user to input width and height 
+and prints a checkerboard of 3-by-3 squares. 
+(It should work even if the input dimensions 
+are not a multiple of three.)
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int width, height;
+  cout<<"Input width: "<< endl;
+  cin >> width;
+  cout<<"Input height: "<<endl;
+  cin >> height;
+
+  for(int row=0; row<height; row++){
+    for(int col=0; col<width; col++){
+      if((row/3+col/3)%2 == 0){
+        cout<<"*";
+      } else{
+        cout<< " ";
+      }
+    }
+    cout<< endl;
+  }
+  return 0;
+  
+  
+}
+
+
---
+++ sort2.cpp
@@ -0,0 +1,25 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: sort2.cpp
+
+*/
+#include <iostream>
+
+using namespace std;
+
+void sort2(int& a, int& b){
+  if(a>b){
+    int temp=a;
+    a=b;
+    b=temp;
+  };
+}
+
+int main(){
+  int a =3;
+  int b =2;
+  sort2(a,b);
+}
+
