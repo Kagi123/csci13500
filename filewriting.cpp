0ecb67e8faf7e5049d1202c53927d7cc67acee50
unknown <spark26@angelo.edu>
2022-03-04 22:11:49 -0500

    update

---
+++ filewriting.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: filewriting.cpp
+
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    ofstream ofile("hello.txt");
+    ofile << "Hello, World!";
+    ofile.close();
+    ifstream ifile("hello.txt");
+    getline(ifile, str);
+    cout << str << endl;
+}
---
+++ sort3.cpp
@@ -0,0 +1,39 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: sort3.cpp
+
+*/
+#include <iostream>
+
+using namespace std;
+
+void sort3(int& a, int& b, int& c){
+  int temp1 = a;
+  int temp2 = b;
+  int temp3 = c;
+  if(b<a){
+    a = temp2;
+    b = temp1;
+  }
+  if(c<b){
+    if(a>c){
+      c = b;
+      b = a;
+      a = temp3;
+    } else {
+      c =b;
+      b = temp3;
+    }
+  }
+  
+}
+
+int main(){
+  int v=4;
+  int w =2;
+  int x = 3;
+  sort3(v,w,x);
+  cout<<v<<w<<x;
+}
