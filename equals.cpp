a303a1bed1facfa88801cdbf923e77a138477c0e
unknown <spark26@angelo.edu>
2022-03-09 20:30:16 -0500

    update

---
+++ equals.cpp
@@ -0,0 +1,33 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: equals.cpp
+
+checks whether two arrays 
+have the same elements 
+in the same order.
+*/
+#include<iostream>
+
+using namespace std;
+
+bool equals(int a[],int a_size, int b[],int b_size){
+  if(a_size == b_size){
+    int j=0;
+    for(int i=0; i<a_size; i++){
+      if(a[i] == b[i]) j++;
+    }
+    if(j == a_size) return true;
+    else return false;
+  } else return false;
+}
+
+
+
+int main(){
+  int a[] = {1,2,3,4};
+  int b[] = {1,2,3};
+  cout<<equals(a,4,b,3);
+  
+}
