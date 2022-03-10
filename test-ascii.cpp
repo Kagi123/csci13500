15163c898c2dfd50a4b576d539261170d5ea7ac3
unknown <spark26@angelo.edu>
2022-03-10 10:49:46 -0500

    update

---
+++ test-ascii.cpp
@@ -0,0 +1,21 @@
+﻿/*
+Author: sangheum Park
+Course: CSCI-13500
+Instructor: Tong Yi
+Assignment: test-ascii.cpp
+
+report all characters from 
+the input line together with their ASCII codes.
+*/
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  cout<<"Input: "<<endl;
+  string s;
+  getline(cin, s);
+  for(int i=0; i<s.length(); i++){
+    cout << s[i] << " " << int(s[i])<< endl;
+  }
+}
