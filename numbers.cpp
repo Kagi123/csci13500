1ca8a20b00258677e443789e5a8fd93d2ae788d1
unknown <spark26@angelo.edu>
2022-03-16 23:46:39 -0400

    update

--- numbers.cpp
+++ numbers.cpp
@@ -13,49 +13,20 @@ is a twin prime?
 Largest twin prime in range
 */
 #include<iostream>
+#include <math.h>
 
 using namespace std;
 
-bool isDivisibleBy(int n, int d){
-  return n%d == 0;
-  
-}
 bool isPrime(int n){
-   for(int i = 2; i < n-1; i++)
+   for(int i = 2; i <sqrt(n); i++)
         if(n % i == 0) return false;
     return true;
 }
 
-int nextPrime(int n){
-  int i = 1;
-  while(true){
-  if(isPrime(n+i)) return n+i;
-    else i++;
-    }
-}
-
-int countPrimes(int a, int b){
-  int prime_num = 0;
-  for(int i =a; i<b+1; i++){
-      if(isPrime(i)==true) prime_num +=1;
-  }
-  return prime_num;
-}
-
 bool isTwinPrime(int n){
-  if(isPrime(n) == true){
-    if(isPrime(n-2) == true || isPrime(n+2) == true )
+  if(isPrime(n) == true && (isPrime(n-2) == true || isPrime(n+2) == true))
       return true;
-    else return false;
-  } else return false;
-}
-
-int nextTwinPrime(int n){
-  int i=1;
-  while(true){
-    if(isTwinPrime(n+i)) return n+i;
-    else i++;
-  }
+  else return false;
 }
 
 int largestTwinPrime(int a, int b){
@@ -64,17 +35,4 @@ int largestTwinPrime(int a, int b){
       if(isTwinPrime(i)== true) twinprime = i;
   }
   return twinprime;
-}
-
-
-int main(){
-  int a,b;
-  cout<<"Enter a integer1: "<<endl;
-  cin>>a;
-  cout<<"Enter a integer2: "<<endl;
-  cin >> b;
-  if(largestTwinPrime(a,b) == -1) {
-    cout<<"There's no largestTwinPrime"<<endl;
-  } else
-  cout<<"lagestTwinPrime: "<<largestTwinPrime(a,b);
 }
