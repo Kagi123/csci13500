275d9c55dea4b5fd5ff864d6ad8ca7bf67dd2766
unknown <spark26@angelo.edu>
2022-03-16 23:50:46 -0400

    update

--- numbers.cpp
+++ numbers.cpp
@@ -12,15 +12,35 @@ countprime
 is a twin prime?
 Largest twin prime in range
 */
+
 #include<iostream>
-#include <math.h>
 
 using namespace std;
 
+bool isDivisibleBy(int n, int d){
+  return n%d == 0;
+  
+}
 bool isPrime(int n){
-   for(int i = 2; i <sqrt(n); i++)
+   for(int i = 2; i < n-1; i++)
         if(n % i == 0) return false;
     return true;
+}
+
+int nextPrime(int n){
+  int i = 1;
+  while(true){
+  if(isPrime(n+i)) return n+i;
+    else i++;
+    }
+}
+
+int countPrimes(int a, int b){
+  int prime_num = 0;
+  for(int i =a; i<b+1; i++){
+      if(isPrime(i)==true) prime_num +=1;
+  }
+  return prime_num;
 }
 
 bool isTwinPrime(int n){
@@ -31,14 +51,18 @@ bool isTwinPrime(int n){
   } else return false;
 }
 
+int nextTwinPrime(int n){
+  int i=1;
+  while(true){
+    if(isTwinPrime(n+i)) return n+i;
+    else i++;
+  }
+}
+
 int largestTwinPrime(int a, int b){
   int twinprime = -1;
   for(int i =a; i<b+1; i++){
       if(isTwinPrime(i)== true) twinprime = i;
   }
   return twinprime;
-}
-
-int main(){
-  cout<<largestTwinPrime(0,15485661);
 }
