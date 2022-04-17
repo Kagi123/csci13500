c8283cfa993bfcc83b151a58ef7ccec168db265d
unknown <spark26@angelo.edu>
2022-04-17 11:37:57 -0400

    u

--- 3d-space.cpp
+++ 3d-space.cpp
@@ -3,11 +3,9 @@ Author: sangheum Park
 Course: CSCI-13500
 Instructor: Tong Yi
 Assignment: 3d-space.cpp
-receives the coordinates of two points 
-(passed as pointers), and returns 
-the pointer of the point that 
-is farther away from the origin.
-
+add functions that create, 
+delete, and coordinate 
+objects dynamically:
 */
 
 #include <iostream>
@@ -37,14 +35,34 @@ void move(Coord3D *ppos, Coord3D *pvel, double dt){
   ppos->z = ppos->z + pvel->z * dt;
 }
 
-int main() {
-    Coord3D pos = {0, 0, 100.0};
-    Coord3D vel = {1, -5, 0.2};
-
-    move(&pos, &vel, 2.0); // object pos gets changed
-    cout << pos.x << " " << pos.y << " " << pos.z << endl;
-    // prints: 2 -10 100.4
+Coord3D* createCoord3D(double x, double y, double z){
+        Coord3D *myCoord = new Coord3D;
+        Coord3D coord = {x, y, z};
+        *myCoord = coord;
+    return myCoord;
 }
 
+void deleteCoord3D(Coord3D *p){
+    delete p;
+}
+
+int main() {
+    double x, y, z;
+    cout << "Enter position: ";
+    cin >> x >> y >> z;
+    Coord3D *ppos = createCoord3D(x,y,z);
+    
+    cout << "Enter velocity: ";
+    cin >> x >> y >> z;
+    Coord3D *pvel = createCoord3D(x,y,z);
+
+    move(ppos, pvel, 10.0);
+
+    cout << "Coordinates after 10 seconds: " 
+         << (*ppos).x << " " << (*ppos).y << " " << (*ppos).z << endl;
+
+    deleteCoord3D(ppos); // release memory
+    deleteCoord3D(pvel);
+}
 
 
